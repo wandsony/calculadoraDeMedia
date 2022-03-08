d20899cc35ab8910295d59dae27dc96ffa9b22d9
Wandson Gomes <wandsony@gmail.com>
2022-03-08 13:20:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var nome = "Wandson"
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre = 4
+var notaDoQuartoBimestre = 2
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(0)
+
+console.log("Bem vindo " + nome)
+console.log(notaFixada) 
+
+// isso é um comentario
+
+// Revisão
+// variaveis, strings, console.log, toFixed, operações matemáticas, concatenação
